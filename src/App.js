5d83234c6eaffd546e205ec46bf662442cb4b262
afshin <afshinpht1379@gmail.com>
2022-11-11 20:11:00 +0330

    add tailwindcss to project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import "./App.css";
 import Navbar from "./pages/navbar/component/Navbar";
 import LoginPage from "./pages/loginPage/component/LoginPage";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/login" element={<LoginPage />}></Route>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App bg-gray-100">
+            <Navbar></Navbar>
+            <Routes>
+                <Route path="/login" element={<LoginPage/>}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
