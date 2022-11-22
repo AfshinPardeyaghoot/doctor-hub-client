d8900b128994c58cbf084bd84492b817029f7e9a
afshinpy <afshinpht1379@gmail.com>
2022-11-22 06:40:53 +0000

    add profile dropdown

--- src/page/navbar/component/Navbar.js
+++ src/page/navbar/component/Navbar.js
@@ -11,7 +11,11 @@ function Navbar() {
     const [isUserLogin, setIsUserLogin] = useState(false);
     const [getUserInfoReg, response] = useAuthRequest()
     const [username, setUsername] = useState(null);
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
 
+    const handleProfileIsOpen = () => {
+        setIsProfileOpen(!isProfileOpen);
+    }
 
     useEffect(() => {
 
@@ -37,11 +41,34 @@ function Navbar() {
 
             {!isUserLogin
                 ? <div className="header_navbar_login_btn" onClick={() => navigate("/login")}>ورود</div>
-                : <div
-                    className="space-x-1 flex columns-1 absolute justify-between top-2 h-1/2 left-5 content-center text-xs text-gray-600 border-double border-2 border-green-400 border-double rounded bg-green-400 hover:border-white p-0 m-0">
-                    {username && <div className="m-0.5">{username}</div>}
-                    <img className="w-4 h-4" src={avatar} alt="avatar-icon"/>
-                </div>}
+                :
+                <div>
+                    <button onClick={handleProfileIsOpen}
+                            className="fixed space-x-1 flex columns-1 absolute justify-between top-2 h-1/2 left-5 content-center text-xs text-gray-600 border-double border-2 border-green-400 border-double rounded bg-green-400 hover:border-white p-0 m-0 items-center p-2 text-sm text-gray-600 border-transparent rounded-md focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring dark:text-white dark:bg-gray-800 focus:outline-none">
+                        <img className="w-4 h-4" src={avatar} alt="avatar-icon"/>
+                        <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M12 15.713L18.01 9.70299L16.597 8.28799L12 12.888L7.40399 8.28799L5.98999 9.70199L12 15.713Z"
+                                fill="currentColor"></path>
+                        </svg>
+
+                    </button>
+                    {
+                        isProfileOpen &&
+                        <div className="flex absolute columns-1 w-40 h-40 top-8 space-x-1 left-5  text-xs text-gray-600 border-double border-2 border-green-400 border-double rounded bg-green-400 hover:border-white overflow-hidden bg-white rounded-md shadow-xl dark:bg-gray-800">
+                            <div
+                                className="flex absolute w-40 h-7 items-center text-sm text-gray-600 transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white">
+                                <div className="mx-1 flex absolute w-40 justify-center text-center text-sm font-semibold text-gray-700 dark:text-gray-200">
+                                    {username}
+                                </div>
+                            </div>
+                        </div>
+                    }
+                </div>
+
+
+            }
+
 
             <div className="header_navbar_logo_container">
                 <img className="header_navbar_logo" src={logo} alt="DOCTOR HUB"></img>
