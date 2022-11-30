e80fa8fb1850def8b198698c20db9bca6b2e8085
afshinpy <afshinpht1379@gmail.com>
2022-11-30 09:53:59 +0000

    response login js

--- src/page/authentication/login/Login.js
+++ src/page/authentication/login/Login.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import "react-phone-number-input/style.css";
 import logo from "../../../static/logo/doctor-hub-green.png";
+import backgroundLogo from "../../../static/logo/doctor-hub-background.png";
 import "./Login.css";
 import {useNavigate} from "react-router-dom";
 import useRequest from "../../../hook/useRequest";
@@ -69,28 +70,28 @@ function LoginPage() {
     }
 
     return (
-        <div className="relative bg-white h-full items-center flex flex-col md:bg-gray-100">
+        <div className="relative bg-white items-center flex flex-col xl:flex-row xl:bg-[rgb(76,228,130)]">
             <div
-                className="flex justify-center bg-white max-w-screen-sm md:shadow relative  w-screen flex-col text-red-900 items-center content-center">
-                <div className="relative bg-[rgb(76,228,130)] w-full h-20 flex justify-center rounded-b-3xl">
+                className="flex xl:left-0 justify-center bg-white max-w-screen-sm relative  w-screen flex-col text-red-900 items-center content-center">
+                <div className="relative bg-[rgb(76,228,130)] xl:hidden w-full h-20 flex justify-center rounded-b-3xl xl:rounded-b-none">
                     <img className="relative h-16 w-16 top-2 flex items-center content-center" src={logo}
                          alt="DOCTOR HUB"></img>
                 </div>
-                <div className="relative flex  h-[85vh] md:h-[91vh]  w-5/6 content-center items-center">
+                <div className="relative flex h-[88vh] xl:h-[100vh] w-5/6 content-center items-center">
                     <div className="relative w-screen h-full flex items-center flex-col">
-                        <label className="text-gray-700 relative top-14 md:top-40 font-semibold text-l font-cambria">به
+                        <label className="text-gray-700 relative top-14 xl:top-40 font-semibold text-right w-full text-l font-cambria">به
                             دکتر هاب
                             خوش
                             آمدید</label>
                         <label
-                            className="text-gray-600 relative top-16 md:top-44 text-m font-light py-4 w-36 md:w-full">شماره
+                            className="text-gray-600 relative top-16 xl:top-44 text-s font-light py-4 w-full text-right xl:w-full">شماره
                             تلفن همراه
                             خود را برای دریافت کد
                             تائید وارد
                             کنید</label>
 
                         <div
-                            className="phoneError text-red-500 py-1 relative top-20 md:top-56 h-10 text-m font-light pb-8">
+                            className="phoneError text-red-500 py-1 text-right w-full relative top-20 xl:top-56 h-10 text-m font-light pb-8">
                             {hasError &&
                                 <span>شماره همراه وارد شده معتبر
                                 نمی باشد!</span>
@@ -98,20 +99,23 @@ function LoginPage() {
                         </div>
 
                         <input
-                            className="relative bg-gray-100 border-double border-gray-200 border-2 top-20 md:top-56 w-4/5 h-16 border-solid text-center rounded border-gray-400 text-slate-600"
+                            className="relative bg-gray-100 border-double border-gray-200 border-2 top-28 xl:top-56 w-full h-14 border-solid text-center rounded border-gray-400 text-slate-600"
                             placeholder="شماره همراه با 09"
                             type="text"
                             onChange={(e) => handlePhoneChange(e.target.value)}
                             autoFocus={true}
                         ></input>
                         <button
-                            className="relative mb-10 bg-green-400 text-white m-4 top-20 md:top-56 w-4/5 h-16 border-double border-green-400 border-2  hover:border-2  hover:border-double hover:border-white text-center rounded text-slate-600"
+                            className="relative mb-10 bg-green-400 text-white m-4 top-28 xl:top-56 w-full h-14 border-double border-green-400 border-2  hover:border-2  hover:border-double hover:border-white text-center rounded text-slate-600"
                             onClick={() => handleSubmit()}>
                             تایید شماره همراه
                         </button>
-                        <ToastContainer className="toast bg-white"/>
+                        <ToastContainer className="toast bg-white justify-center te"/>
                     </div>
                 </div>
+            </div>
+            <div className="bg-[rgb(76,228,130)] invisible relative flex content-center h-0 w-0 justify-center items-center xl:h-screen xl:w-screen xl:visible">
+                <img src={backgroundLogo} className="relative invisible xl:visible xl:h-3/5 xl:w-3/5" alt=""/>
             </div>
         </div>
     );
