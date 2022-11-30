26b29c8f46a3fff37f6fb4c60abcca8caba4bd2d
afshin <afshinpht1379@gmail.com>
2022-11-30 22:52:04 +0330

    fix issues

--- src/page/navbar/Navbar.js
+++ src/page/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useCallback, useEffect, useState} from "react";
 import "./Navbar.css";
-import logo from "../../static/logo/doctor_hub.png";
+import logo from "../../static/logo/doctor-hub-green.png";
 import avatar from '../../static/icon/avatar-icon.png'
 import {useNavigate} from "react-router-dom";
 import useAuthRequest from "../../hook/useAuthRequest";
@@ -45,15 +45,15 @@ function Navbar() {
 
 
     return (
-        <div className="relative h-24 w-100 flex justify-between items-center shadow">
-            <div className="w-56 h-24">
+        <div className="relative h-24 w-100 flex justify-between bg-green-400 items-center shadow rounded-b">
+            <div className="w-56 h-24 bg-green-400">
                 <div className="relative w-56 h-24 flex flex-col">
                     {isUserLogin ?
                         <button data-collapse-toggle="navbar-default" type="button"
                                 className="inline-flex relative items-center top-[5vh] pl-5 ml-3 text-sm text-gray-500 rounded-lg md:hidden"
                                 aria-controls="navbar-default" aria-expanded="false" onClick={handleNabvarShow}>
                             <span className="sr-only">Open main menu</span>
-                            <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            <svg className="w-10 h-10" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                                  xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                       d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
@@ -61,7 +61,7 @@ function Navbar() {
                             </svg>
                         </button> :
                         <div
-                            className="inline-flex bg-green-400 w-20 h-10 top-[3vh] relative items-center  pl-5 ml-3 text-sm text-gray-500 rounded-lg md:hidden"
+                            className="inline-flex bg-gray-500 w-20 h-10 top-[3vh] relative items-center  pl-5 ml-3 text-sm text-gray-300 border-double border-2 border-gray-300 hover:border-gray-50 rounded-lg md:hidden"
                             onClick={() => navigate("/login")}>ورود
                         </div>
                     }
@@ -69,10 +69,10 @@ function Navbar() {
                     {isNavbarOpen && <div
                         className="flex relative w-56 pl-5  top-[6vh] justify-center  items-center  md:block md:w-auto"
                         id="navbar-default">
-                        <ul className="flex flex-col right-0 relative w-60  border border-gray-100 rounded-lg bg-green-400 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
+                        <ul className="flex flex-col right-0 bg-gray-50 relative w-60  border border-gray-100 rounded-lg bg-green-400 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
                             <li>
                                 <button onClick={handleProfileIsOpen}
-                                        className="relative h-12 sw-20 bg-green-400 rounded flex justify-center items-center text-white">
+                                        className="relative h-12 sw-20 rounded flex justify-center items-center text-white">
                                     <div
                                         className="flex flex-col w-40 text-sm text-gray-600 transition-colors duration-300 transform">
                                         <div
@@ -105,8 +105,8 @@ function Navbar() {
 
                 </div>
             </div>
-            <div className="relative bg-blue-300 right-5">
-                <img className="header_navbar_logo" src={logo} alt="DOCTOR HUB"></img>
+            <div className="relative right-5 bg-green-400">
+                <img className="relative h-16 w-16 top-2 flex items-center content-center" src={logo} alt="DOCTOR HUB"></img>
             </div>
         </div>
     );
