857efce8c1965c45adcb23b00468741dea96ff21
afshinpy <afshinpht1379@gmail.com>
2023-01-10 10:26:29 +0000

    fix issues

--- src/page/authentication/login/Login.js
+++ src/page/authentication/login/Login.js
@@ -118,7 +118,7 @@ function LoginPage() {
                 </div>
             </div>
             <div
-                className="bg-neutral-800 invisible relative flex content-center h-0 w-0 justify-center items-center xl:h-screen xl:w-screen xl:visible">
+                className="bg-emerald-500 invisible relative flex content-center h-0 w-0 justify-center items-center xl:h-screen xl:w-screen xl:visible">
                 <img src={logo} className="relative object-cover invisible w-3/5 max-w-[500px] xl:visible " alt=""/>
             </div>
         </div>
