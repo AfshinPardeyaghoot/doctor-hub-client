02ee98f48fbce186c15f2f104c60fa6c8763f6fc
afshinpy <afshinpht1379@gmail.com>
2023-01-14 10:25:31 +0000

    fix edit info page style

--- src/page/editInfo/EditInfo.js
+++ src/page/editInfo/EditInfo.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import backIcon from '../../static/icon/back.png';
 import useAuthRequest from "../../hook/useAuthRequest";
 import ApiRoutes from "../../config/ApiRoutes";
 
@@ -20,16 +21,20 @@ function EditInfo() {
         if (firstName === '') {
             setHasFirstNameError(true)
             setErrorMsg('نام خود را وارد کنید.')
+        } else {
+            setHasFirstNameError(false)
+            setFirstName(firstName);
         }
-        setFirstName(firstName);
     }
 
-    const handleLastNameChange = () => {
+    const handleLastNameChange = (lastName) => {
         if (lastName === '') {
             setHasLastNameError(true)
             setErrorMsg('نام خانوادگی خود را وارد کنید.')
+        } else {
+            setHasLastNameError(false)
+            setLastName(lastName)
         }
-        setLastName(lastName)
     }
 
     useEffect(() => {
@@ -46,15 +51,61 @@ function EditInfo() {
     }, [])
 
     return (
-        <div className="flex flex-col justify-center items-start p-5">
-            <input className='w-full bg-n text-end border-[1px] border-solid border-gray-300 p-4' type={"text"}
-                   onChange={(e) => handleFirstNameChange(e.target.value)}
-                   defaultValue={user.firstName}/>
-            <input type={"text"}
-                   onChange={(e) => handleLastNameChange(e.target.value)}
-                   defaultValue={user.lastName}/>
-            <input type={"text"} value={user.phone}/>
-            <button>ثبت تغییرات</button>
+        <div className='flex flex-col items-center w-full h-full'>
+            <div className="flex flex-col justify-between items-start p-5 w-full max-w-screen-lg h-full">
+                <div className='w-full flex flex-col'>
+                    <div className=''>
+                        <a className='w-2 h-2'>
+                            <img className='w-6 h-6' src={backIcon} alt={'error'}/>
+                        </a>
+                    </div>
+
+                    <label className="w-full flex justify-start text-gray-700 text-[15px] mt-24 mb-2 px-2.5 rtl"
+                           htmlFor='firstName'>
+                        نام<span className='text-red-400 px-1'>*</span>
+                    </label>
+                    <input id='firstName'
+                           className={!hasFirstNameError ? 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none transition ease-in-out focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 py-4 rtl'
+                               : 'bg-gray-50 border border-red-500 text-gray-900 text-sm rounded-lg focus:outline-none transition ease-in-out focus:ring-emerald-500 focus:border-red-500 block w-full p-2.5 py-4 rtl'}
+                           type={"text"}
+                           onChange={(e) => handleFirstNameChange(e.target.value)}
+                           defaultValue={user.firstName}/>
+
+                    <div
+                        className={hasFirstNameError ? 'w-full flex justify-start rtl px-2.5 py-1 text-[12px] text-red-600' : 'w-full invisible justify-start rtl px-2.5 py-1 text-[12px] text-red-600'}>نام
+                        خود را وارد
+                        کنید.
+                    </div>
+
+
+                    <label className="w-full flex justify-start text-gray-700 text-[15px] mb-2 px-2.5 pt-5 rtl"
+                           htmlFor='lastName'>
+                        نام خانوادگی<span className='text-red-400 px-1'>*</span>
+                    </label>
+                    <input id='lastName'
+                           className={!hasLastNameError ? 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none transition ease-in-out focus:ring-emerald-500 focus:border-emerald-500 block w-full p-2.5 py-4 rtl'
+                               : 'bg-gray-50 border border-red-500 text-gray-900 text-sm rounded-lg focus:outline-none transition ease-in-out focus:ring-emerald-500 focus:border-red-500 block w-full p-2.5 py-4 rtl'}
+                           type={"text"}
+                           onChange={(e) => handleLastNameChange(e.target.value)}
+                           defaultValue={user.lastName}/>
+
+                    <div
+                        className={hasLastNameError ? 'w-full flex justify-start rtl px-2.5 py-1 text-[12px] text-red-600' : 'w-full invisible justify-start rtl px-2.5 py-1 text-[12px] text-red-600'}>
+                        نام خانوادگی خود را وارد کنید.
+                    </div>
+
+                    <label className="w-full flex justify-start text-gray-700 text-[15px] mb-2 px-2.5 pt-5 rtl"
+                           htmlFor='phone'>
+                        شماره تلفن<span className='text-red-400 px-1'>*</span>
+                    </label>
+                    <input id='phone' readOnly
+                           className={'bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:outline-none transition ease-in-out block w-full p-2.5 py-4 rtl'}
+                           type={"text"}
+                           defaultValue={user.phone}/>
+
+                </div>
+                <button className='p-4 rounded-lg w-full text-center bg-emerald-500 text-white'>ثبت تغییرات</button>
+            </div>
         </div>
     )
 }
